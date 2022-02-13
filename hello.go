bd974d544c81b9276dc2191d68c4a6ad9db3b16d
Dimitri <dimitrilw@gmail.com>
2022-02-13 16:11:14 -0700

    multiple languages enabled

--- hello.go
+++ hello.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 const (
+	spanish         string = "spanish"
+	spanishGreeting string = "Hola"
 	englishGreeting string = "Hello"
 )
 
-func Hello(name string) string {
+func getGreeting(language string) string {
+	switch strings.ToLower(language) {
+	case spanish:
+		return spanishGreeting
+	default:
+		return englishGreeting
+	}
+
+}
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return fmt.Sprintf("%s, %s", englishGreeting, name)
+	return fmt.Sprintf("%s, %s", getGreeting(language), name)
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }

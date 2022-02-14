ce7fd62ec345eeda8edffb34cbc42b2302bf6ea0
Dimitri Langston <dimitri@silverthreadinc.com>
2022-02-14 12:01:02 -0700

    init repeat files

---
+++ iteration/repeat_test.go
@@ -0,0 +1,26 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("repeats string", func(t *testing.T) {
+		got := Repeat("x")
+		want := "xxxxx"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func ExampleRepeat() {
+	fmt.Println(Repeat("x"))
+	// Output: xxxxx
+}

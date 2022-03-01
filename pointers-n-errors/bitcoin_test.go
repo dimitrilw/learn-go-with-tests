df85f8d6c3c020d9b551f61d237ac5f81c7d91ba
bunny <bunnybunnygame@gmail.com>
2022-03-01 12:06:45 -0700

    refactor tests

--- pointers-n-errors/bitcoin_test.go
+++ pointers-n-errors/bitcoin_test.go
@@ -3,24 +3,24 @@ package pointersnerrors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	t.Run("Deposit", func(t *testing.T) {
-		w := Wallet{}
-		w.Deposit(Bitcoin(10))
-		got := w.Balance()
-		want := Bitcoin(10)
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		w := Wallet{}
+		w.Deposit(Bitcoin(10))
+		assertBalance(t, w, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		w := Wallet{balance: Bitcoin(20)}
 		w.Withdraw(Bitcoin(10))
-		got := w.Balance()
-		want := Bitcoin(10)
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, w, Bitcoin(10))
 	})
 }
 

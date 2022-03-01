73642f1383b22d8731f703a1d956f85ac0701370
bunny <bunnybunnygame@gmail.com>
2022-03-01 12:18:13 -0700

    add check of error message to assertError

--- pointers-n-errors/bitcoin_test.go
+++ pointers-n-errors/bitcoin_test.go
@@ -2,20 +2,24 @@ package pointersnerrors
 
 import "testing"
 
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+func assertError(t testing.TB, got error, want string) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+	if got.Error() != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
 func TestWallet(t *testing.T) {
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-	assertError := func(t testing.TB, err error) {
-		t.Helper()
-		if err == nil {
-			t.Fatal("wanted an error but didn't get one")
-		}
-	}
 	t.Run("Deposit", func(t *testing.T) {
 		w := Wallet{}
 		w.Deposit(Bitcoin(10))
@@ -30,7 +34,7 @@ func TestWallet(t *testing.T) {
 		startingBalance := Bitcoin(20)
 		w := Wallet{startingBalance}
 		err := w.Withdraw(Bitcoin(100))
-		assertError(t, err)
+		assertError(t, err, "non-sufficient funds; amount exceeds Wallet balance")
 		assertBalance(t, w, startingBalance)
 	})
 }

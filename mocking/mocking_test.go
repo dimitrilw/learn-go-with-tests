893a5a1b29b32c8df85651b236563c07e092f120
Dimitri <dimitrilw@gmail.com>
2022-03-04 15:35:34 -0700

    implement mock & spy on time slept

--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/lithammer/dedent"
 )
@@ -42,6 +43,17 @@ func TestCountdown(t *testing.T) {
 	})
 }
 
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
+}
+
 type SpySleeper struct {
 	Calls int
 }
@@ -64,4 +76,12 @@ func (s *SpyCountdownOperations) Sleep() {
 func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 	s.Calls = append(s.Calls, write)
 	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }

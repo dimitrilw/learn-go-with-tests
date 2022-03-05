2663235852a3d81381a0226f06b84673a62d2545
Dimitri <dimitrilw@gmail.com>
2022-03-04 17:01:47 -0700

    tidy code

---
+++ select/webspeed_test.go
@@ -0,0 +1,33 @@
+package learnselect
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	defer slowServer.Close()
+
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}

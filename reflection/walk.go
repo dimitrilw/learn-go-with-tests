5cf18dc7a82dbe7056483fb6a4cc1ceda4a9dcaa
Dimitri <dimitrilw@gmail.com>
2022-03-07 17:53:46 -0700

    handle string, struct, slice, and array

---
+++ reflection/walk.go
@@ -0,0 +1,44 @@
+package reflection
+
+import "reflect"
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
+}
+
+type walkElements struct {
+	numElements   int
+	getElementVia func(int) reflect.Value
+	walkFn        func(input string)
+}
+
+func (w walkElements) walkThem() {
+	for i := 0; i < w.numElements; i++ {
+		walk(w.getElementVia(i).Interface(), w.walkFn)
+	}
+}
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		walkElements{
+			numElements:   val.NumField(),
+			getElementVia: val.Field,
+			walkFn:        fn,
+		}.walkThem()
+	case reflect.Slice, reflect.Array:
+		walkElements{
+			numElements:   val.Len(),
+			getElementVia: val.Index,
+			walkFn:        fn,
+		}.walkThem()
+	}
+}
---
+++ reflection/walk_test.go
@@ -0,0 +1,93 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	testTable := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Boulder"},
+			},
+			[]string{"London", "Boulder"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Boulder"},
+			},
+			[]string{"London", "Boulder"},
+		},
+	}
+
+	for _, tt := range testTable {
+		t.Run(tt.Name, func(t *testing.T) {
+			var got []string
+			walk(tt.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, tt.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, tt.ExpectedCalls)
+			}
+		})
+	}
+}
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
